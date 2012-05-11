3f8a275746c5bdc5ab18e443f646f994ccb9ea5e
Michel Kuhlmann <michel@kuhlmanns.info>
2012-05-11 13:13:47 +0200

    fixed bug, when run without cmd-args

--- R/procCli.R
+++ R/procCli.R
@@ -5,29 +5,31 @@ procCli <-
 )
 {
 
-    library('getopt')
-    opt_spec <- 
-        matrix(c(
-                 'help',     'h', 0, "logical",     "Getting help"
-               , 'week',     'w', 0, "character", "Directoy of week to summarize"
-               , 'month',     'm', 0, "character", "Directoy of month to summarize"
-               , 'year',     'y', 0, "character", "Directoy of month to summarize"
-                 ), ncol = 5, byrow = TRUE)
-    opt <- getopt(spec = opt_spec)
+    if ( identical(args, character(0)) ) 
+    {  
+        list(
+             week = strftime(Sys.Date(), format = "%Y/%m/%W")
+           , month = strftime(Sys.Date(), format = "%Y/%m")
+           , year = strftime(Sys.Date(), format = "%Y")
+             )
+    } else 
+    {
+        library('getopt')
+        opt_spec <- 
+            matrix(c(
+                     'help',     'h', 0, "logical",     "Getting help"
+                   , 'week',     'w', 0, "character", "Directoy of week to summarize"
+                   , 'month',     'm', 0, "character", "Directoy of month to summarize"
+                   , 'year',     'y', 0, "character", "Directoy of month to summarize"
+                     ), ncol = 5, byrow = TRUE)
+        opt <- getopt(spec = opt_spec)
 
-    if ( !is.null(opt$help) || is.null(commandArgs()) )   {    
-        cat(getopt(spec = opt_spec, usage = TRUE, command = "summarizeWorkHours"))
-        q(status=0)
+        if ( !is.null(opt$help) || is.null(commandArgs()) )   {    
+            cat(getopt(spec = opt_spec, usage = TRUE, command = "summarizeWorkHours"))
+            q(status=0)
+        }
+        opt$ARGS <- NULL
+        opt            # Alist with command-line arguments
     }
-    if ( all(sapply(opt, function(x) is.null(x))) ) {  
-        opt <- 
-            list(
-                 week = strftime(Sys.Date(), format = "%Y/%m/%W")
-               , month = strftime(Sys.Date(), format = "%Y/%m")
-               , year = strftime(Sys.Date(), format = "%Y")
-                 )
-    }
-    opt$ARGS <- NULL
-    opt            # Alist with command-line arguments
     ### Returns a named list with given command-line arguments
 }
