ebfef0c7ca839236f1e67f40acfa811a085f2614
Michel Kuhlmann <michel@kuhlmanns.info>
2014-05-20 09:49:58 +0200

    excluding columns beeing all zero

--- R/procProjectHours.R
+++ R/procProjectHours.R
@@ -6,22 +6,31 @@ procProjectHours <-
         lapply(
             projs, function(pn)
         {
-            p <-                                # project-data
+            p <-                        # project-data
                 subset(x, ProjectNb == pn)
-            do.call(
-                rbind
-                , lapply(                             # hours subproject per worker
-                         levels(factor(p$ProjectSubNb)), function(spn)
-                     {
-                         sp <-
-                             subset(p, ProjectSubNb == spn)
-                         wh <-                    # hours per worker
-                             sapply(
-                                 levels(sp$Person), function(wn)
-                                 sum((subset(sp, Person == wn)$Time)))
-                         data.frame(t(wh), row.names = spn)
-                     }
-                         )
+            dd <-
+                do.call(
+                    rbind
+                  , lapply(               # hours subproject per worker
+                      levels(factor(p$ProjectSubNb)), function(spn)
+                      {
+                          sp <-
+                              subset(p, ProjectSubNb == spn)
+                          wh <-          # hours per worker
+                              sapply(
+                                  levels(sp$Person), function(wn)
+                                      sum((subset(sp, Person == wn)$Time)))
+                          data.frame(t(wh), row.names = spn)
+                      }
+                  )
                 )
+            if (is.null(dd))
+                NULL
+           else
+           {
+               i <-                        # skip columns all being 0
+                   as.logical(apply(dd, 2, function(r) all(sapply(r, function(x) x != 0))))
+               subset(dd, select  = i)
+           }
         }), projs)
 }
