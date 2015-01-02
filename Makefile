d4daa5fb0c3123df8e307f13b10fdda4b17f05b6
Michel Kuhlmann <michel@kuhlmanns.info>
2015-01-02 07:23:13 +0000

    dump

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 .PHONY: rd
 rd :
 	../annotSrcDoc/exec/annotatedR2doc -m man -f R/*.R
+
+.PHONY: install
+
+install:
+	R CMD INSTALL .
--- default.nix
+++ default.nix
@@ -1,16 +1,12 @@
 let                                                                                                                                                                    
-                                                                                                                                                                       
   pkgs = import <nixpkgs> {};                                                                                                                                          
   buildRPackage = import <nixpkgs/pkgs/development/r-modules/generic-builder.nix> pkgs.R;                                                                              
-                                                                                                                                                                       
 in                                                                                                                                                                     
-                                                                                                                                                                       
 with pkgs.rPackages;                                                                                                                                                   
-                                                                                                                                                                       
 {                                                                                                                                                                      
   logbuch = buildRPackage {
-    name = "r-logbuch-1.1";
+    name = "logbuch-1.1";
     src = ./.;
-    propagatedBuildInputs = [ggplot2 reshape2 getopt optparse yaml];
+    propagatedBuildInputs = [ggplot2 reshape2 getopt optparse yaml xlsx];
   };
 }
