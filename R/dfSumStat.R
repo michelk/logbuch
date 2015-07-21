4e2ece7a83cb8405e723a2243bb92fbc4d5d16b2
Michel Kuhlmann <michel@kuhlmanns.info>
2015-07-21 07:16:28 +0200

    extractProjectHours: option to output wide table

--- R/dfSumStat.R
+++ R/dfSumStat.R
@@ -4,6 +4,6 @@ dfSumStat <-
     x                                   # ^ data.frame
 )
 {
-    cs <- cbind(x, Total = rowSums(x))
-    rbind(cs, Total = colSums(cs))
+    cs <- cbind(x, Total = rowSums(x, na.rm = TRUE))
+    rbind(cs, Total = colSums(cs, na.rm = TRUE))
 }                              # ^ data.frame with column and row sums
